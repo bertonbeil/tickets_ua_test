fbcf4a56d795463d48904c7ca4ba7e1e6e4d6c18
Andrew Dmutriv <Andrew Dmutriv>
2016-10-27 01:57:17 +0300

    yeah! perfect pixel desktop version and and fourth cup of coffee

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     console.info('jQuery included');
 
+    var mySwiper = new Swiper ('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+    });
+
 });
